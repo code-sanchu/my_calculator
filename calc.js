8cd01ceb6629dd8d92b4e80378598bc7ae2e8826
RubenCcode <35168325+RubenCcode@users.noreply.github.com>
2018-04-13 16:53:54 +0100

    change operator functionality added

--- calc.js
+++ calc.js
@@ -6,6 +6,7 @@ var input = [];
 var nextOperation = '';
 var temp = 0;
 var answer = 0;
+var flag = false;
 
 function operate() {
   if (nextOperation === '+') {
@@ -21,104 +22,124 @@ function operate() {
   }
 }
 
-
-
 $('#1').click(function () {
   input.push(1);
   $('#answer').text(Number(input.join('')));
+  flag = true;
 })
 
 $('#2').click(function () {
   input.push(2);
   $('#answer').text(Number(input.join('')));
+  flag = true;
 })
 
 $('#3').click(function () {
   input.push(3);
   $('#answer').text(Number(input.join('')));
+  flag = true;
 })
 
 $('#4').click(function () {
   input.push(4);
   $('#answer').text(Number(input.join('')));
+  flag = true;
 })
 
 $('#5').click(function () {
   input.push(5);
   $('#answer').text(Number(input.join('')));
+  flag = true;
 })
 
 $('#6').click(function () {
   input.push(6);
   $('#answer').text(Number(input.join('')));
+  flag = true;
 })
 
 $('#7').click(function () {
   input.push(7);
   $('#answer').text(Number(input.join('')));
+  flag = true;
 })
 
 $('#8').click(function () {
   input.push(8);
   $('#answer').text(Number(input.join('')));
+  flag = true;
 })
 
 $('#9').click(function () {
   input.push(9);
   $('#answer').text(Number(input.join('')));
+  flag = true;
 })
 
 $('#0').click(function () {
   input.push(0);
   $('#answer').text(Number(input.join('')));
+  flag = true;
 })
 
 $('#plus').click(function () {
-  if (nextOperation !== '=') {
-    temp = Number(input.join(''));
-    operate();
+  if (flag) {
+    if (nextOperation !== '=') {
+      temp = Number(input.join(''));
+      operate();
+    }
+    input = [];
+    $('#temp').text(answer);
+    $('#answer').text('0');
   }
-  input = [];
   nextOperation = '+';
-  $('#temp').text(answer);
-  $('#answer').text('0');
   $('#operator').text('+');
+  flag = false;
 })
 
 $('#multiply').click(function () {
-  if (nextOperation !== '=') {
-    temp = Number(input.join(''));
-    operate();
+  if (flag) {
+    if (nextOperation !== '=') {
+      temp = Number(input.join(''));
+      operate();
+    }
+    input = [];
+    $('#temp').text(answer);
+    $('#answer').text('0');
   }
-  input = [];
   nextOperation = '*';
-  $('#temp').text(answer);
-  $('#answer').text('0');
   $('#operator').text('*');
+  flag = false;
 })
 
 $('#minus').click(function () {
-  if (nextOperation !== '=') {
-    temp = Number(input.join(''));
-    operate();
+  if (flag) {
+    if (nextOperation !== '=') {
+      temp = Number(input.join(''));
+      operate();
+    }
+    input = [];
+    $('#temp').text(answer);
+    $('#answer').text('0');
   }
-  input = [];
   nextOperation = '-';
-  $('#temp').text(answer);
-  $('#answer').text('0');
   $('#operator').text('-');
+  flag = false;
 })
 
 $('#divide').click(function () {
-  if (nextOperation !== '=') {
-    temp = Number(input.join(''));
-    operate();
+  if (flag) {
+    if (nextOperation !== '=') {
+      temp = Number(input.join(''));
+      operate();
+    }
+    input = [];
+    $('#temp').text(answer);
+    $('#answer').text('0');
   }
-  input = [];
   nextOperation = '/';
-  $('#temp').text(answer);
-  $('#answer').text('0');
   $('#operator').text('/');
+  flag = false;
 })
 
 $('#equal').click(function () {
